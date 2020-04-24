b7cf79f6214ea5ba5ddb7f43ad4aa313184c654d
github username <github email address>
2020-04-24 13:57:06 -0400

    Done.

--- lib/project.rb
+++ lib/project.rb
@@ -11,16 +11,27 @@ class Project
 
 
     #Instance methods
-
+    
+    #create
     def add_backer(backer)
         ProjectBacker.new( self, backer )
     end
-
+    
+    #read
     def backers
         pbs = ProjectBacker.all.select {|pb| pb.project == self }
         pbs.map { |pb| pb.backer }
     end
 
+    #delete
+    def remove_backer(backer)
+        pbs_to_delete = ProjectBacker.all.find {|pb| pb.project == self && pb.backer == backer}
+        ProjectBacker.all.delete(pbs_to_delete)
+    end
 
+    #update
+    def update_backer
+        pbs_to_delete = ProjectBacker.all.find {|pb| pb.project == self && pb.backer == backer}
+    end
 
 end
